a5b96507813ef48ac29aa860fe8d5a21653bed6c
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-12-01 20:15:14 -0200

    Add bootstrap dependency

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,8 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
