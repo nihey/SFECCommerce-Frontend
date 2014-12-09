476c9aded7da70fb2f1daabb4881e244c215ae94
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-12-08 23:04:41 -0200

    Fix localStorage cart corruption

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -7,7 +7,8 @@ export default Ember.Controller.extend({
     actions: {
         add_cart: function(product) {
             if (product.is_available) {
-                var cart = JSON.parse(localStorage['cart']) || [];
+                localStorage['cart'] = localStorage['cart'] || '[]';
+                var cart = JSON.parse(localStorage['cart']);
                 cart.push(product);
                 localStorage['cart'] = JSON.stringify(cart);
             }
